bd3e725de30597d5d54b79f705a9c1e6bed3ebec
PovilasU <povilas.urbonas85@gmail.com>
2025-05-29 23:04:52 +0300

    purge css

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import { visualizer } from "rollup-plugin-visualizer";
 export default defineConfig({
   base: "/justfortoday-preact-app/", // Uncomment this line if you want to serve from a subdirectory
   plugins: [preact(), visualizer({ open: false })],
+
   // build: {
   //   sourcemap: false, // ⬅️ This disables source map generation
   // },
