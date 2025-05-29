38c3e3bf3ed088bfb3403b703c24602f9a2a649e
PovilasU <povilas.urbonas85@gmail.com>
2025-05-29 19:27:57 +0300

    updated base

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "justfortoday-preact-app", // Uncomment this line if you want to serve from a subdirectory
+  base: "/justfortoday-preact-app/", // Uncomment this line if you want to serve from a subdirectory
   plugins: [preact(), visualizer({ open: false })],
   // build: {
   //   sourcemap: false, // ⬅️ This disables source map generation
